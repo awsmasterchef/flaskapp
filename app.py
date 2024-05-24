c37797a3e2dc1d323bce427be74b302c27b20610
awsmasterchef <143173187+awsmasterchef@users.noreply.github.com>
2024-05-24 17:31:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def contact():
+    heading = "Connect With Me"
+    return render_template('contact.html', heading=heading)
+
+
+@app.route('/test')
+def test():
+    return "test"
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+    # Handle the received data here (e.g., save it to a database, send an email, etc.)
+    return jsonify(message=f'Thank you for your message, {name}!')
+
+
+@app.route('/visitors')
+def another_page():
+    total_visitors = 100
+    return render_template('viewer_count.html', total_visitors=total_visitors)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
