2abb0139744e687476875bf1bcf36fedc628f4a3
awsmasterchef <143173187+awsmasterchef@users.noreply.github.com>
2024-05-29 18:01:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+import awsgi
 
 app = Flask(__name__)
 
@@ -28,5 +29,9 @@ def another_page():
     return render_template('viewer_count.html', total_visitors=total_visitors)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+def lambda_handler(event, context):
+    return awsgi.response(app, event, context, base64_content_types={"image/png"})
